891f469da0a229a55fa0e4694c432ef696d66f31
Daniels <daniel.sadowski75@gmail.com>
2018-12-05 22:40:12 +0100

    Added search, album, artist, track components and routs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,31 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchComponent } from './search/search.component';
+import { AlbumComponent } from './album/album.component';
+import { TrackComponent } from './track/track.component';
+import { ArtistComponent } from './artist/artist.component';
+import {RouterModule, Routes} from "@angular/router";
+
+const routs: Routes = [
+  {path: 'search', component: SearchComponent},
+  {path: 'artist', component: ArtistComponent},
+  {path: 'track', component: TrackComponent},
+  {path: 'album', component: AlbumComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    AlbumComponent,
+    TrackComponent,
+    ArtistComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routs)
   ],
   providers: [],
   bootstrap: [AppComponent]
