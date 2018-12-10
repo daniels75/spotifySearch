fe3d761b2409fe10c9f3419be50ef6184b8172f7
Daniels <daniel.sadowski75@gmail.com>
2018-12-10 20:51:24 +0100

    Step 5.1 search input field

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-search',
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
-
+  @Input() search: string;
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onClick(search: HTMLInputElement,) {
+    console.log('search value: ', search.value);
   }
 
 }
