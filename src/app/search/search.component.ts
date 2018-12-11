cc7113e61be39099644677654b1da0d3fe088439
Daniels <daniel.sadowski75@gmail.com>
2018-12-11 21:37:15 +0100

    Step 5.8 added new properties to Artist, changed a display results

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -43,10 +43,14 @@ export class SearchComponent implements OnInit {
       let resultItems: any = queryResults.tracks.items;
       resultItems.map(item => {
         let artist: Artist = new Artist();
-        artist.name = item.name;
         artist.id = item.id;
+        artist.name = item.name;
+        artist.artistId = item.artists[0].id;
+        artist.artistName = item.artists[0].name;
         artist.trackNumber = item.track_number;
-        artist.imageUrl = item.album.images[0];
+        artist.imageUrl = item.album.images[1].url;
+        artist.albumId  = item.album.id;
+        artist.albumName = item.album.name;
 
         this.results.push(artist);
         console.log('artist: ', artist);
