ea59dbd0daf411f9bf47eb9a1a7f94ff311b7c25
Daniels <daniel.sadowski75@gmail.com>
2018-12-12 23:43:04 +0100

    6.3 updated a track page - now value are displayed

--- src/app/track/track.component.ts
+++ src/app/track/track.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, Router} from "@angular/router";
+import {SpotifyService} from "../spotify.service";
+import {Location} from "@angular/common";
 
 @Component({
   selector: 'app-track',
@@ -6,10 +9,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./track.component.css']
 })
 export class TrackComponent implements OnInit {
+  private id: any;
+  private track: any;
 
-  constructor() { }
+
+  constructor(private activatedRoute: ActivatedRoute,
+              private spotifyService: SpotifyService,
+              private location: Location) {
+    this.activatedRoute.params.subscribe(
+      (params: any) => this.showParams(params)
+    );
+    this.activatedRoute.paramMap.subscribe(
+    (params: any) => this.showMap(params)
+    );
+  }
+
+  showParams(params: any): void{
+    console.log(`params: ${params}`);
+
+    this.spotifyService.findTracks(params['id']).subscribe(
+      (results: any) => {
+        console.log(`Track results: ${results}`)
+        this.renderTrack(results);
+      }
+    )
+  }
+
+  renderTrack(results: any): void {
+    this.track = results;
+  }
+  showMap(params: any): void{
+    console.log(`params: ${params}`);
+  }
+
+  back(): void {
+    this.location.back();
+  }
 
   ngOnInit() {
   }
+  
 
 }
+
+
